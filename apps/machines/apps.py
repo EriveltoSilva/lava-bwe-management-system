e1d1c8f74275f431acacb1fc9b1a04e99f39fac5
Erivelto Silva <eriveltoclenio@gmail.com>
2024-10-13 14:32:58 -0300

    feat: machines app

---
+++ apps/machines/apps.py
@@ -0,0 +1,8 @@
+"""machines app configuration"""
+
+from django.apps import AppConfig
+
+
+class MachinesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.machines"
