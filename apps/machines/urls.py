a531c1643f289ec7b2002a3330ddac5b2ebdb68d
Erivelto Silva <eriveltoclenio@gmail.com>
2024-10-14 14:32:39 -0300

    feat(machine state): list, create and delete
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   apps/machines/urls.py
    	modified:   apps/machines/views.py
    	modified:   db.sqlite3
    	new file:   templates/machines/partials copy/_modal_delete.html
    	new file:   templates/machines/partials copy/_modal_delete_state_machine.html
    	new file:   templates/machines/partials/_modal_delete_state_machine.html
    	new file:   templates/machines/register-state.html
    	new file:   templates/machines/state_list.html
    	modified:   templates/partials/_sidebar.html

--- apps/machines/urls.py
+++ apps/machines/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path("deletar-maquina/<slug:slug>", views.delete, name="delete"),
     path("lista-de-maquinas", views.list_machine, name="list"),
     path("detalhes/<slug:slug>", views.details, name="details-json"),
+    #
+    path("estados-de-maquinas/novo-estado", views.register_state, name="register-state"),
+    path("estados-de-maquinas/deletar/<uuid:mid>", views.delete_machine_states, name="delete-state"),
+    path("estados-de-maquinas", views.list_states, name="list-states"),
 ]
--- apps/machines/views.py
+++ apps/machines/views.py
@@ -7,10 +7,91 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views import View
 
-from .forms import MachineForm
+from .forms import MachineForm, MachineStateForm
 from .models import Machine, MachineState
 
 NUMBER_OF_MACHINE = 15
+
+
+class RegisterMachineStateView(View):
+    """view for RegisterMachine"""
+
+    template_name = "machines/register-state.html"
+    form_class = MachineStateForm
+
+    def get(self, request, *args, **kwargs):
+        """render form for register new machine state"""
+        form = self.form_class(data=request.session.get("machine_state_form_data", None))
+        return render(
+            request, self.template_name, {"form": form, "menu_page": "machines", "sub_page": "register-state"}
+        )
+
+    def post(self, request, *args, **kwargs):
+        """post data for register new machine state"""
+        request.session["machine_state_form_data"] = request.POST
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Novo estado de Máquina registado com sucesso!")
+            del request.session["machine_state_form_data"]
+        else:
+            messages.error(request, "Formulário com dados inválidos!")
+
+        previous_page = request.META.get("HTTP_REFERER")
+        return HttpResponseRedirect(previous_page or "")
+
+
+register_state = RegisterMachineStateView.as_view()
+
+
+class DeleteMachineStateView(View):
+    """View for delete machines"""
+
+    def get_object(self, *args, **kwargs):
+        """get machine object"""
+        mid = self.kwargs.get("mid")
+        return MachineState.objects.get(mid=mid)
+
+    def post(self, request, *args, **kwargs):
+        """delete a machine"""
+        try:
+            machine_state = self.get_object(*args, **kwargs)
+            machine_state.delete()
+            messages.success(request, "Estado deletado com sucesso!")
+        except MachineState.DoesNotExist as e:
+            messages.error(request, f"Estado da Máquina não encontrada: {e}")
+        return redirect(request.META.get("HTTP_REFERER") or "")
+
+
+delete_machine_states = DeleteMachineStateView.as_view()
+
+
+class ListStateMachine(View):
+    """View for list machine states"""
+
+    template_name = "machines/state_list.html"
+
+    def get(self, request, *args, **kwargs):
+        """list machines state"""
+        list_machine_states = MachineState.objects.all()
+        paginator = Paginator(list_machine_states, NUMBER_OF_MACHINE)
+        page = request.GET.get("page")
+        machine_states = paginator.get_page(page)
+
+        return render(
+            request,
+            self.template_name,
+            {"machine_states": machine_states, "menu_page": "machines", "sub_page": "state_list"},
+        )
+
+    def post(self, request, *args, **kwargs):
+        state_name = request.POST.get("name")
+        MachineState.objects.create(name=state_name)
+        messages.success(request, "Novo estado da máquina cadastrado com sucesso!")
+        return redirect(reverse("state_list"))
+
+
+list_states = ListStateMachine.as_view()
 
 
 def details(request, slug):
@@ -109,20 +190,20 @@ class EditMachineView(View):
     def get(self, request, *args, **kwargs):
         """edit a machine"""
         machine = self.get_object(*args, **kwargs)
-        form = self.form_class(data=request.session.get("machine_form_data", None), instance=machine)
+        form = self.form_class(data=request.session.get("edit_machine_form_data", None), instance=machine)
         return render(request, self.template_name, {"form": form, "menu_page": "machines", "sub_page": "edit"})
 
     def post(self, request, *args, **kwargs):
         """edit a machine post data"""
         machine = self.get_object(*args, **kwargs)
-        request.session["machine_form_data"] = request.POST
+        request.session["edit_machine_form_data"] = request.POST
         form = self.form_class(data=request.POST, instance=machine)
 
         if form.is_valid():
             machine = form.save(commit=False)
             machine.save()
             messages.success(request, "Máquina editada com sucesso!")
-            del request.session["machine_form_data"]
+            del request.session["edit_machine_form_data"]
         else:
             messages.error(request, "Erro ao editar a máquina!")
         return HttpResponseRedirect(request.META.get("HTTP_REFERER") or "")
@@ -139,11 +220,12 @@ class RegisterMachinesView(View):
 
     def get(self, request, *args, **kwargs):
         """registers a machine"""
-        form = self.form_class()
+        form = self.form_class(data=request.session.get("machine_form_data", None))
         return render(request, self.template_name, {"form": form, "menu_page": "machines", "sub_page": "register"})
 
     def post(self, request, *args, **kwargs):
         """registers a machine post data"""
+        request.session["machine_form_data"] = request.POST
         form = self.form_class(request.POST)
         if form.is_valid():
             # save form data
@@ -151,6 +233,7 @@ class RegisterMachinesView(View):
             machine.created_by = request.user
             machine.save()
             messages.success(request, "Máquina registada com sucesso!")
+            del request.session["machine_form_data"]
         else:
             messages.error(request, "Usuário não encontrado!")
 
